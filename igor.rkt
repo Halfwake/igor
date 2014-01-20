7b6c4a1709ed9c791cf4af57524d44bac5be4979
unknown <drewdudash@gmail.com>
2014-01-19 19:13:15 -0500

    first commit, working as of now

---
+++ igor.rkt
@@ -0,0 +1,48 @@
+#lang racket
+
+(require (for-syntax racket))
+(provide (all-defined-out))
+
+(struct message (value sender))
+
+(define (send-message routine value)
+  (thread-send routine (message value (current-thread))))
+
+(define-syntax (define-routine stx)
+  (define top-form (syntax->datum stx))
+  (match-define (list _ name-id state-constructor-id message-value-id state-id alarm-id sender-id) (take top-form 7))
+  (define (compile-send-form send-form)
+    `(send-message ,sender-id ,(second send-form)))
+  (define (compile-recurse-form recurse-form)
+    (match (length recurse-form)
+      [2 `(recurse-procedure ,(second recurse-form) ,alarm-id)]
+      [3 `(recurse-procedure ,(second recurse-form) ,(third recurse-form))])) ;; Should give useful error message if wrong form length.
+  (define (compile-on-form-with-send-and-recurse forms)
+    (define match-value (second forms))
+    (define body (drop forms 2))
+    (define send-forms (filter (compose (curry eq? 'send) first)
+                               body))
+    (define recurse-forms (filter (compose (curry eq? 'recurse) first)
+                                  body))
+    `[,match-value
+      ,@(append (map compile-send-form send-forms)
+                (map compile-recurse-form recurse-forms))])
+  (define body (drop top-form 7)) ;; All forms should be pair? , all forms should start with 'recurse or 'send ?
+  (define on-forms (filter (compose (curry eq? 'on)
+                                    first)
+                           body))
+  (define on-alarm-forms (filter (compose (curry eq? 'on-alarm) first)
+                                 body))
+  (define compiled-body `(match ,alarm-id
+                           ,@(map compile-on-form-with-send-and-recurse on-alarm-forms)
+                           [_ (match ,message-value-id
+                                ,@(map compile-on-form-with-send-and-recurse on-forms)
+                                [_ (recurse-procedure ,state-id 'no-matching-clause)])]))
+  (datum->syntax stx
+                 `(define (,name-id initial-state)
+                    (define (recurse-procedure ,state-id ,alarm-id)
+                      (match-define (message ,message-value-id ,sender-id) (thread-receive))
+                      ,compiled-body)
+                    (thread (lambda ()
+                              (recurse-procedure initial-state null))))))
+                            
---
+++ test1.rkt
@@ -0,0 +1,32 @@
+#lang racket
+
+(require "igor.rkt")
+
+(struct fridge (door-open food-amount))
+
+(define (fridge-close old-fridge)
+  (struct-copy fridge old-fridge [door-open #f]))
+
+(define (fridge-open old-fridge)
+  (struct-copy fridge old-fridge [door-open #t]))
+
+(define (fridge-open? old-fridge)
+  (fridge-door-open old-fridge))
+
+(define-routine fridge-routine fridge message-value state alarm sender
+  (on-alarm 'bad-sender-value
+    (recurse state null))
+  (on 'open
+    (recurse (fridge-open state)))
+  (on 'close
+    (recurse (fridge-close state)))
+  (on 'open?
+    (send (fridge-open? state))
+    (recurse state)))
+
+(define f (fridge-routine (fridge #f 0)))
+(send-message f 'open?)
+(message-value (thread-receive))
+(send-message f 'open)
+(send-message f 'open?)
+(message-value (thread-receive))
